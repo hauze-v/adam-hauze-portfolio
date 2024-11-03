ed3a7b0e323edacd02c4c7ac9ac9bcdc2d7785cd
hauze-v <hauze_v@protonmail.com>
2024-11-03 15:47:37 -0500

    Most of the static html done

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,108 @@
-import Image from "next/image";
+import Head from "next/head";
+import { BsFillMoonStarsFill } from 'react-icons/bs';
+import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
+import Image from 'next/image';
+import adamPic from '../../public/adam-headshot.jpeg';
+import adamWave from '../../public/adam-wave.png';
+import design from '../../public/design.png';
+import code from '../../public/code.png';
+import tools from '../../public/tools.png';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <Head>
+        <title>Adam Hauze's Portfolio</title>
+        <meta name="description" content="An online resume and project portfolio site for Adam Hauze" />
+      </Head>
+      <main className="px-10">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        {/* Hero section */}
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-2xl font-burtons">adamhauze</h1>
+            <ul className="flex items-center">
+              <li><BsFillMoonStarsFill className="cursor-pointer text-xl" /></li>
+              <li>
+                <a href="https://www.linkedin.com/in/adam-hauze-bbbb7083/"
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8">
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium">Adam Hauze</h2>
+            <h3 className="text-2xl py-2">Developer and designer.</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">I specialize in frontend frameworks, crafting pixel-perfect web components and maintaining high testing standards through comprehensive unit and end-to-end testing strategies. My background in cybersecurity allows me to approach frontend development with a security-minded perspective, ensuring robust and secure applications.</p>
+          </div>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <a href="https://github.com/hauze-v">
+              <AiFillGithub />
+            </a>
+            <a href="https://www.linkedin.com/in/adam-hauze-bbbb7083/">
+              <AiFillLinkedin />
+            </a>
+          </div>
+          <div className="relative bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 mx-auto overflow-hidden">
+            <Image src={adamWave} alt="Profile picture of Adam Hauze" layout="fill" objectFit="cover" />
+          </div>
+        </section>
+
+        {/*  */}
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">Services I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              As a web developer, I specialize in crafting seamless digital experiences using modern technologies. My expertise lies in frontend frameworks like <span className="text-teal-500">Angular</span> and <span className="text-teal-500">React</span>, enhanced by <span className="text-teal-500">Typescript</span> for reliable, scalable solutions. On the backend, I architect powerful server solutions using <span className="text-teal-500">Node.js</span> and <span className="text-teal-500">Firebase</span>, creating efficient systems that scale with your business needs.
+            </p>
+            <p>
+              Beyond development, I bring a keen eye for design and user experience. Through UI/UX design and prototyping with <span className="text-teal-500">Figma</span>, I transform ideas into polished, professional web applications that not only function flawlessly but deliver an intuitive and engaging user experience that keeps your customers coming back.
+            </p>
+          </div>
+          <div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image className="mx-auto" src={design} width={100} height={100} alt="3D blender style icon that represents a list of design skills" />
+              <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
+              <p className="py-2">Creating elegant designs suited for your needs using design theory</p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Canva</p>
+              <p className="text-gray-800 py-1">Figma</p>
+              <p className="text-gray-800 py-1">Generate Blocks - Wordpress</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image className="mx-auto" src={code} width={100} height={100} alt="3D blender style icon that represents a list of design skills" />
+              <h3 className="text-lg font-medium pt-8 pb-2"></h3>
+              <p className="py-2">Creating elegant designs suited for your needs using design theory</p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Canva</p>
+              <p className="text-gray-800 py-1">Figma</p>
+              <p className="text-gray-800 py-1">Generate Blocks - Wordpress</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image className="mx-auto" src={consulting} width={100} height={100} alt="3D blender style icon that represents a list of design skills" />
+              <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
+              <p className="py-2">Creating elegant designs suited for your needs using design theory</p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Canva</p>
+              <p className="text-gray-800 py-1">Figma</p>
+              <p className="text-gray-800 py-1">Generate Blocks - Wordpress</p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">Portfolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              As a web developer, I specialize in crafting seamless digital experiences using modern technologies. My expertise lies in frontend frameworks like <span className="text-teal-500">Angular</span> and <span className="text-teal-500">React</span>, enhanced by <span className="text-teal-500">Typescript</span> for reliable, scalable solutions. On the backend, I architect powerful server solutions using <span className="text-teal-500">Node.js</span> and <span className="text-teal-500">Firebase</span>, creating efficient systems that scale with your business needs.
+            </p>
+            <p>
+              Beyond development, I bring a keen eye for design and user experience. Through UI/UX design and prototyping with <span className="text-teal-500">Figma</span>, I transform ideas into polished, professional web applications that not only function flawlessly but deliver an intuitive and engaging user experience that keeps your customers coming back.
+            </p>
+          </div>
+
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
